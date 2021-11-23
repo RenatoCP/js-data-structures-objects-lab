14dd88f1cfa128e7c5e089fb7a48e985be625036
RenatoCP <renato_pinha@icloud.com>
2021-11-23 11:44:57 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const driver = {name: 'Sam'}
+
+const updateDriverWithKeyAndValue = (driver, key, value) => {
+    return Object.assign({}, driver, {[key]: value});
+}
+
+const address = updateDriverWithKeyAndValue(driver, 'address', '11 Broadway')
+
+const destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) => {
+    driver[key] = value;
+    return driver
+}
+
+const deleteFromDriverByKey = (driver, key) => {
+    let newDriver = {...driver}
+
+    newDriver[key] = undefined
+
+    return newDriver
+}
+
+const destructivelyDeleteFromDriverByKey = (driver, key) => {
+    driver[key] = undefined
+
+    return driver
+}
